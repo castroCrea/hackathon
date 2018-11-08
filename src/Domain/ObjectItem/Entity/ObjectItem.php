98d40149c1947fa73f7ab557b0f1809cc2eab080
KarloHRZ <kperduv@gmail.com>
2018-11-08 22:14:54 +0100

    Entities OBjectItem, Protection, Weapon

---
+++ src/Domain/ObjectItem/Entity/ObjectItem.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Domain\ObjectItem\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ApiResource(
+ *     collectionOperations={"post"={
+ *              "method"="POST",
+ *              "normalization_context"={"groups"={"post"}},
+ *              "denormalization_context"={"groups"={"post"}}
+ *      },
+ *
+ *     "get"={
+ *              "method"="GET",
+ *              "normalization_context"={"groups"={"get"}}
+ *      }
+ *     },
+ *     itemOperations={"get"={"method"="GET"}}
+ * )  itemOperations={"get"}
+ * )
+ */
+class ObjectItem
+{
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Groups({"post", "get"})
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @Groups({"post", "get"})
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @Groups({"post", "get"})
+     * @var int
+     */
+    private $value;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function setValue(int $value): void
+    {
+        $this->value = $value;
+    }
+
+
+
+
+}
