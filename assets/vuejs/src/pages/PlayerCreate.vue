96ee61247235ff219af983dabc08245b324d845f
paolo <paolo@bird-iffice.com>
2018-11-16 15:30:56 +0100

    create form vue JS

--- assets/vuejs/src/pages/PlayerCreate.vue
+++ assets/vuejs/src/pages/PlayerCreate.vue
@@ -2,6 +2,25 @@
   <div>
     Formulaire de création de joueurs : WIP
   </div>
+  <form>
+
+    <div class="form-group">
+      <label for="title">Nom</label>
+      <input type="text" class="form-control" id="title" v-model="user.title" name="title" placeholder="Kayviiiine">
+    </div>
+
+    <div class="form-group">
+      <label for="description">Description</label>
+      <textarea class="form-control" id="description" v-model="user.description" name="description"></textarea>
+    </div>
+
+    <div class="form-group">
+      <label for="maxPlayer">Nombre maximum de joueurs</label>
+      <input type="number" class="form-control" id="maxPlayer" name="maxPlayer" v-model.number="user.maxPlayer" placeholder="100">
+    </div>
+
+    <button type="submit" v-on:click.prevent="createPlayers()" class="btn btn-primary">Valider</button>
+  </form>
 </template>
 
 <script>
@@ -17,9 +36,12 @@
                 }
             }
         },
+        mounted() {
+            this.user.game = '/api/games/'+this.$router.params.gameId;
+        },
         methods: {
-            createGame(e) {
-                this.axios.post('http://localhost:8000/api/games', this.user)
+            createPlayers(e) {
+                this.axios.post('http://localhost:8000/api/players', this.user)
                 .then((response) => {
                   this.$router.push({name: 'Home'})
                 })
