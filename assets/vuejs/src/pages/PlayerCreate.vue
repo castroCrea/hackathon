f8cc893f05d1adc2391b10347ee1d75e69251695
regis <regis@bird-office.com>
2018-11-18 16:17:27 +0100

    Player Create update css style

--- assets/vuejs/src/pages/PlayerCreate.vue
+++ assets/vuejs/src/pages/PlayerCreate.vue
@@ -10,19 +10,19 @@
             <div class="form-group">
               <div class="custom-control custom-radio custom-control-inline">
                 <input type="radio" id="playerGender-m" name="gender" value="m" v-model="$v.gender.$model" class="custom-control-input"
-                       :class="[ $v.gender.$error ? 'is-invalid' : 'is-valid' ]"
+                       :class="{'is-invalid' : $v.gender.$error }"
                 >
                 <label class="custom-control-label" for="playerGender-m">Homme</label>
               </div>
               <div class="custom-control custom-radio custom-control-inline">
                 <input type="radio" id="playerGender-f" name="gender" value="f" v-model="$v.gender.$model" class="custom-control-input"
-                       :class="[ $v.gender.$error ? 'is-invalid' : 'is-valid' ]"
+                       :class="{'is-invalid' : $v.gender.$error }"
                 >
                 <label class="custom-control-label" for="playerGender-f">Femme</label>
               </div>
               <div class="custom-control custom-radio custom-control-inline">
                 <input type="radio" id="playerGender-none" name="gender" value="none" v-model="$v.gender.$model" class="custom-control-input"
-                       :class="[ $v.gender.$error ? 'is-invalid' : 'is-valid' ]"
+                       :class="{'is-invalid' : $v.gender.$error }"
                 >
                 <label class="custom-control-label" for="playerGender-none">¯\_(ツ)_/¯</label>
               </div>
@@ -30,28 +30,30 @@
             <div class="form-group">
               <label for="playerName">Nom</label>
               <input type="text" id="playerName" name="name" v-model="$v.name.$model" class="form-control" placeholder="Nom"
-                     :class="[ $v.name.$error ? 'is-invalid' : 'is-valid' ]"
+                     :class="{'is-invalid' : $v.name.$error }"
               >
             </div>
             <div class="form-group">
               <label for="playerPicture">Photo</label>
               <div class="custom-file">
-                <input type="file" class="custom-file-input" id="playerPicture" @change="imageToBase64String" name="picture" aria-describedby="Photo">
+                <input type="file" class="custom-file-input" id="playerPicture" @change="imageToBase64String" name="picture" aria-describedby="Photo"
+                       :class="{'is-invalid' : !this.picture }"
+                 >
                 <label class="custom-file-label" for="playerPicture">Inserer une image</label>
               </div>
-              <img :src="picture" class="my-4 d-block border-primary border p-3" alt="" height="250" v-if="picture">
+              <img :src="picture" class="my-4 d-block border-primary border p-3" alt="" height="250" v-if="picture.trim()">
 
             </div>
             <div class="form-group">
               <label for="playerRace">Race</label>
               <input type="text" id="playerRace" class="form-control" name="player.race" v-model="$v.race.$model" placeholder="Race"
-                     :class="[ $v.race.$error ? 'is-invalid' : 'is-valid' ]"
+                     :class="{'is-invalid' : $v.race.$error }"
               >
             </div>
             <div class="form-group">
               <label for="playerJob">Metier</label>
               <input type="text" id="playerJob" class="form-control" name="job" v-model="$v.job.$model" placeholder="Metier"
-                     :class="[ $v.job.$error ? 'is-invalid' : 'is-valid' ]"
+                     :class="{'is-invalid' : $v.job.$error }"
               >
             </div>
             <div class="custom-control custom-checkbox custom-control-inline form-group">
@@ -76,18 +78,18 @@
         name: "PlayerCreate",
         data(){
             return {
-                game: "",
-                gender: "",
+                game: "/api/game/",
+                gender: "/api/gender/",
                 name: "",
-                picture: false,
-                race: "",
-                job: "",
+                picture: ' ',
+                race: "/api/race/",
+                job: "/api/job/",
                 gameMaster: "false",
                 submitStatus: null
             }
         },
         created() {
-            this.game = '/api/games/' + this.$route.params.gameId
+            this.game = this.game + this.$route.params.gameId
         },
         methods: {
             createPlayer(e) {
@@ -103,24 +105,19 @@
                       .then((response) => {
                           this.submitStatus = 'SUCCESS'
                           this.$router.push({name: 'Home'})
+                          console.log(response);
                       })
                 }
 
             },
             imageToBase64String(e) {
-                var input = event.target;
-                // Ensure that you have a file before attempting to read it
+                const input = event.target;
                 if (input.files && input.files[0]) {
-                    // create a new FileReader to read this image and convert to base64 format
                     var reader = new FileReader();
-                    // Define a callback function to run, when FileReader finishes its job
+                    reader.readAsDataURL(input.files[0])
                     reader.onload = (e) => {
-                        // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
-                        // Read image as base64 and set to imageData
-                        this.playerPicture = e.target.result;
+                        this.picture = e.target.result
                     }
-                    // Start the reader job - read file as a data url (base64 format)
-                    reader.readAsDataURL(input.files[0]);
                 }
             },
         },
