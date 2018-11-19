d842d44be597f3270edf0a06821e1e114e166b2c
regis <regis@bird-office.com>
2018-11-19 01:06:50 +0100

    Game Create - Add validation form

--- assets/vuejs/src/pages/GameCreate.vue
+++ assets/vuejs/src/pages/GameCreate.vue
@@ -5,26 +5,34 @@
 
       <div class="form-group">
         <label for="title">Nom</label>
-        <input type="text" class="form-control" id="title" v-model="user.title" name="title" placeholder="Kayviiiine">
+        <input type="text" class="form-control" id="title" v-model="$v.user.title.$model" name="title" placeholder="Kayviiiine"
+         :class="{'is-invalid' : $v.user.title.$error }"
+        >
       </div>
 
       <div class="form-group">
         <label for="description">Description</label>
-        <textarea class="form-control" id="description" v-model="user.description" name="description"></textarea>
+        <textarea class="form-control" id="description" v-model="$v.user.description.$model" name="description"
+         :class="{'is-invalid' : $v.user.description.$error }"
+        ></textarea>
       </div>
 
       <div class="form-group">
         <label for="maxPlayer">Nombre maximum de joueurs</label>
-        <input type="number" class="form-control" id="maxPlayer" name="maxPlayer" v-model.number="user.maxPlayer" placeholder="100">
+        <input type="number" class="form-control" id="maxPlayer" name="maxPlayer" v-model.number="$v.user.maxPlayer.$model" placeholder="100"
+         :class="{'is-invalid' : $v.user.maxPlayer.$error }"
+        >
       </div>
 
 
-      <button type="submit" v-on:click.prevent="createGame()" class="btn btn-primary">Valider</button>
+      <button type="submit" v-on:click.prevent="createGame()" class="btn btn-primary" :disabled="submitStatus === 'PENDING'">Valider</button>
     </form>
   </div>
 </template>
 
 <script>
+
+    import { required } from 'vuelidate/lib/validators'
 
     export default {
         name: "GameCreate",
@@ -34,15 +42,44 @@
                     title: '',
                     description: '',
                     maxPlayer: ''
-                }
+                },
+                submitStatus: null
             }
         },
         methods: {
             createGame(e) {
+
+                this.$v.$touch()
+
+                if (this.$v.$invalid) {
+                    this.submitStatus = 'ERROR'
+                    return false
+                }
+
+                this.submitStatus = 'PENDING'
                 this.axios.post('http://localhost:8000/api/games', this.user)
                 .then((response) => {
+                  this.submitStatus = 'SUCCESS'
                   this.$router.push({name: 'Home'})
                 })
+                .catch((error) => {
+                    this.submitStatus = 'ERROR'
+                    console.log(error)
+                })
+            }
+        },
+
+        validations: {
+            user: {
+                title: {
+                    required
+                },
+                description: {
+                    required
+                },
+                maxPlayer: {
+                    required
+                }
             }
         }
     }
